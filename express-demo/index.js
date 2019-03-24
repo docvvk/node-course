0f02866d6f3752d4e6b929cdc21f3e859f226496
Vivek Malhan <vivek.malhan@yahoo.com>
2019-03-24 18:20:59 -0400

    middleware

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,10 +1,53 @@
 const express = require("express");
 const Joi = require("joi");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const config = require('config');
+const debug = require('debug')('app:startup')
+
+const logger = require("./logger");
+const authenticator = require("./authenticator");
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views'); // default optional
+
+// REQUEST PROCESSING PIPELINE
+
+// ENVIRONMENTS
+
+// console.log(process)
+
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+// console.log(`app: ${app.get("env")}`);
+
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(helmet());
+
+// CONFIGURATION
+console.log('Application Name: ' + config.get('name'))
+console.log('Mail Server Name: ' + config.get('mail.host'))
+console.log('Mail Password: ' + config.get('mail.password'))
+
+
+//TEMPLATING ENGINES
+
+
+// Environments - development, staging, testing or production
+if (app.get("env") === "development") {
+  app.use(morgan("common")); // HTTP request logged in terminal
+ debug("Morgan enabled...");
+}
+
+// DB WORD
+// dbDebugger('connected to db');
+
+app.use(logger);
+app.use(authenticator);
 
 const courses = [
   { id: 1, name: "course1" },
@@ -13,7 +56,7 @@ const courses = [
 ];
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.render('index', {title: 'My Express App', message: 'Hello There Now this is PUG'});
 });
 
 app.get("/api/courses", (req, res) => {
@@ -37,7 +80,7 @@ app.post("/api/courses", (req, res) => {
 
   const course = {
     id: courses.length + 1,
-    name: req.body.name 
+    name: req.body.name
   };
   courses.push(course);
   res.send(course);
@@ -77,3 +120,6 @@ function validateCourse(course) {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// REQUEST PROCESSING PIPELINE
+// Logging , authentication, validation
---
+++ express-demo/public/readme.txt
@@ -0,0 +1,1 @@
+Hi I am just a read me file !
